3902617ec034e28349518d597663ae4294a7529d
hanachiru <hayato.sato@dena.com>
2025-08-31 10:03:29 +0900

    small fix

--- src/AssetBundleMCP/Service/IAnalyzerService.cs
+++ src/AssetBundleMCP/Service/IAnalyzerService.cs
@@ -3,6 +3,5 @@ namespace AssetBundleMcp.Service;
 public interface IAnalyzerService
 {
     string Analyze(string assetBundlePath, string? databaseFilePath = null);
-
-    public void Unload(string databaseFilePath);
+    void Unload(string databaseFilePath);
 }
